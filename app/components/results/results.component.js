343ffe9d0b24937e0e93420740cfc84da4d02d64
Suriel <pokazzam@gmail.com>
2016-10-10 00:06:14 -0500

    updated charts

--- app/components/results/results.component.js
+++ app/components/results/results.component.js
@@ -27,202 +27,277 @@
 		chartFive = angular.element(document.getElementById('chartFive'));
 
 		Highcharts.chart({
-	    chart: {
-	        type: 'column',
-	        renderTo: chartOne[0],
-	        spacingBottom: 0,
-	        spacingTop: 10,
-	        spacingLeft: 25,
-	        spacingRight: 25,
-	    },
-	    title: {
-	        text: ''
-	    },
-	    xAxis: {
-	        categories: ['2011', '2012', '2013', '2014', '2014']
-	    },
-	    yAxis: {
-	        min: 0,
-	        title: {
-	            text: ''
-	        }
-	    },
-	    tooltip: {
-	        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.percentage:.0f}%</b><br/>',
-	        shared: true
-	    },
-	    plotOptions: {
-	        column: {
-	            stacking: 'percent'
-	        }
-	    },
-	    series: [{
-	        name: 'Otros',
-	        data: [65,	54,	63,	54,	68]
-	    },{
-	        name: 'Movilidad',
-	        data: [35,	46,	37,	46,	32]
-	    }]
+			chart: {
+					type: 'column',
+					renderTo: chartOne[0],
+					spacingBottom: 0,
+					spacingTop: 10,
+					spacingLeft: 25,
+					spacingRight: 25,
+			},
+			title: {
+					text: ''
+			},
+			xAxis: {
+					categories: ['2011', '2012', '2013', '2014', '2015']
+			},
+			yAxis: {
+					min: 0,
+					title: {
+							text: ''
+					}
+			},
+			tooltip: {
+					pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.percentage:.0f}%</b><br/>',
+					shared: true
+			},
+			plotOptions: {
+					column: {
+							stacking: 'percent'
+					}
+			},
+			series: [{
+					name: 'Otros',
+					data: [65,	54,	63,	54,	68]
+			},{
+					name: 'Movilidad',
+					data: [35,	46,	37,	46,	32]
+			}]
 		});
 
 		Highcharts.chart({
-	    chart: {
-	        type: 'column',
-	        renderTo: chartTwo[0],
-	        spacingBottom: 0,
-	        spacingTop: 0,
-	        spacingLeft: 25,
-	        spacingRight: 25,
-	    },
-	    title: {
-	        text: 'Stacked column chart'
-	    },
-	    xAxis: {
-	        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-	    },
-	    yAxis: {
-	        min: 0,
-	        title: {
-	            text: 'Total fruit consumption'
-	        }
-	    },
-	    tooltip: {
-	        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
-	        shared: true
-	    },
-	    plotOptions: {
-	        column: {
-	            stacking: 'percent'
-	        }
-	    },
-	    series: [{
-	        name: 'John',
-	        data: [5, 3, 4, 7, 2]
-	    }, {
-	        name: 'Jane',
-	        data: [2, 2, 3, 2, 1]
-	    }, {
-	        name: 'Joe',
-	        data: [3, 4, 4, 2, 5]
-	    }]
+			chart: {
+					type: 'column',
+					renderTo: chartTwo[0],
+					spacingBottom: 0,
+					spacingTop: 10,
+					spacingLeft: 25,
+					spacingRight: 25,
+			},
+			title: {
+					text: ''
+			},
+			xAxis: {
+					categories: ['2011', '2012', '2013', '2014', '2015']
+			},
+			yAxis: {
+					min: 0,
+					title: {
+							text: ''
+					}
+			},
+			tooltip: {
+					pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+					shared: true
+			},
+			plotOptions: {
+					column: {
+							stacking: 'percent'
+					}
+			},
+			series: [
+
+				{
+					name: 'Infraestructura Peatonal',
+					data: [4, 3, 4, 3, 6]
+				},
+				{
+					name: 'Espacio Público',
+					data: [6, 7, 11, 7, 7]
+				},
+				{
+					name: 'Infraestructura Ciclista',
+					data: [0, 0, 0, 0, 0]
+				},
+				{
+					name: 'Transporte Público',
+					data: [10, 29, 10, 7, 1]
+				},
+				{
+					name: 'Pavimentación',
+					data: [32, 34, 34, 27, 35]
+				},
+				{
+					name: 'Infraestructura Vial',
+					data: [48, 26, 40, 56, 51]
+				}
+			]
 		});
 
 		Highcharts.chart({
-	    chart: {
-	        type: 'line',
-	        renderTo: chartThree[0],
-	        spacingBottom: 0,
-	        spacingTop: 0,
-	        spacingLeft: 25,
-	        spacingRight: 25,
-	    },
-	    title: {
-	        text: 'Monthly Average Temperature'
-	    },
-	    subtitle: {
-	        text: 'Source: WorldClimate.com'
-	    },
-	    xAxis: {
-	        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-	    },
-	    yAxis: {
-	        title: {
-	            text: 'Temperature (°C)'
-	        }
-	    },
-	    plotOptions: {
-	        line: {
-	            dataLabels: {
-	                enabled: true
-	            },
-	            enableMouseTracking: false
-	        }
-	    },
-	    series: [{
-	        name: 'Tokyo',
-	        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-	    }, {
-	        name: 'London',
-	        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-	    }]
-	  });
+			chart: {
+						type: 'area',
+						renderTo: chartThree[0],
+						spacingBottom: 0,
+						spacingTop: 10,
+						spacingLeft: 25,
+						spacingRight: 25,
+				},
+				title: {
+						text: ''
+				},
+				subtitle: {
+						text: ''
+				},
+				xAxis: {
+						categories: ['2011', '2012', '2013', '2014', '2015'],
+						tickmarkPlacement: 'on',
+						title: {
+								enabled: true,
+								text: 'Año'
+						}
+				},
+				yAxis: {
+						title: {
+								text: 'Millones'
+						},
+						labels: {
+								formatter: function () {
+									return this.value/1000 + "M";
+								}
+						}
+				},
+				tooltip: {
+						split: true,
+						valueSuffix: ' millones'
+				},
+				plotOptions: {
+						area: {
+								stacking: 'normal',
+								lineColor: '#666666',
+								lineWidth: 1,
+								marker: {
+										lineWidth: 1,
+										lineColor: '#666666'
+								},
+								format: '{point.y:.1f}%'
+						}
+				},
+				series: [{
+						name: 'Otros',
+						data: [69838.03, 53704.75, 43501.24, 35978.96, 36845.53]
+				}, {
+						name: 'Movilidad',
+						data: [38214.89, 46223.97, 25891.65, 31171.16, 17053.72]
+				}]
+		});
 
 		Highcharts.chart({
-	    chart: {
-	        type: 'line',
-	        renderTo: chartFour[0],
-	        spacingBottom: 0,
-	        spacingTop: 0,
-	        spacingLeft: 25,
-	        spacingRight: 25,
-	    },
-	    title: {
-	        text: 'Monthly Average Temperature'
-	    },
-	    subtitle: {
-	        text: 'Source: WorldClimate.com'
-	    },
-	    xAxis: {
-	        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-	    },
-	    yAxis: {
-	        title: {
-	            text: 'Temperature (°C)'
-	        }
-	    },
-	    plotOptions: {
-	        line: {
-	            dataLabels: {
-	                enabled: true
-	            },
-	            enableMouseTracking: false
-	        }
-	    },
-	    series: [{
-	        name: 'Tokyo',
-	        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-	    }, {
-	        name: 'London',
-	        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-	    }]
-	  });
+			chart: {
+						type: 'area',
+						renderTo: chartFour[0],
+						spacingBottom: 0,
+						spacingTop: 10,
+						spacingLeft: 25,
+						spacingRight: 25,
+				},
+				title: {
+						text: ''
+				},
+				subtitle: {
+						text: ''
+				},
+				xAxis: {
+						categories: ['2011', '2012', '2013', '2014', '2015'],
+						tickmarkPlacement: 'on',
+						title: {
+								enabled: true,
+								text: 'Año'
+						}
+				},
+				yAxis: {
+						title: {
+								text: 'Millones'
+						},
+						labels: {
+								formatter: function () {
+									return this.value/1000 + "M";
+								}
+						}
+				},
+				tooltip: {
+						split: true,
+						valueSuffix: ' millones'
+				},
+				plotOptions: {
+						area: {
+								stacking: 'normal',
+								lineColor: '#666666',
+								lineWidth: 1,
+								marker: {
+										lineWidth: 1,
+										lineColor: '#666666'
+								},
+								format: '{point.y:.1f}%'
+						}
+				},
+				series: [
+					{
+						name: 'Infraestructura Peatonal',
+						data: [1532.16, 1572.34, 1043.97, 824.60, 1011.11]
+					},
+					{
+						name: 'Espacio Público',
+						data: [2184.07, 3428.05, 2965.89, 2239.91, 1276.76]
+					},
+					{
+						name: 'Infraestructura Ciclista',
+						data: [101.32, 133.65, 64.35, 142.22, 118.12]
+					},
+					{
+						name: 'Transporte Público',
+						data: [3927.21, 13653.83, 2641.71, 2233.16, 70.47]
+					},
+					{
+						name: 'Pavimentación',
+						data: [12054.99, 16116.62, 8877.96, 8402.31, 5898.17]
+					},
+					{
+						name: 'Infraestructura Vial',
+						data: [18415.14, 11951.31, 10297.78, 17328.96, 8679.09]
+					}
+				]
+		});
 
 		Highcharts.chart({
-	    chart: {
-	        type: 'bar',
-	        renderTo: chartFive[0]
-	    },
-	    title: {
-	        text: 'Stacked bar chart'
-	    },
-	    xAxis: {
-	        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-	    },
-	    yAxis: {
-	        min: 0,
-	        title: {
-	            text: 'Total fruit consumption'
-	        }
-	    },
-	    legend: {
-	        reversed: true
-	    },
-	    plotOptions: {
-	        series: {
-	            stacking: 'normal'
-	        }
-	    },
-	    series: [{
-	        name: 'John',
-	        data: [5, 3, 4, 7, 2]
-	    }, {
-	        name: 'Jane',
-	        data: [2, 2, 3, 2, 1]
-	    }, {
-	        name: 'Joe',
-	        data: [3, 4, 4, 2, 5]
-	    }]
-	  });
+			chart: {
+				type: 'column',
+				renderTo: chartFive[0],
+					spacingBottom: 0,
+					spacingTop: 10,
+					spacingLeft: 25,
+					spacingRight: 25,
+				 },
+				 title: {
+						 text: ''
+				 },
+				 xAxis: {
+						 categories: ['Transporte Público', 'Bicicleta, caminata (espacio público)', 'Automóvil, taxi y otros'],
+						 title: {
+								 text: null
+						 }
+				 },
+				 yAxis: {
+						 min: 0,
+						 title: {
+								 text: ''
+						 },
+						 labels: {
+								 overflow: 'justify'
+						 }
+				 },
+				 tooltip: {
+						 shared: true
+				 },
+
+				 series: [{
+						 name: 'Viajes al trabajo',
+						 data: [45, 24, 31]
+				 }, {
+						 name: 'Inversión',
+						 data: [0, 14, 85]
+				 }]
+		});
 
 
 	}
